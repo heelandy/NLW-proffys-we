ed6d53effd568f17c4a650c99d26969593e480f0
Heelandy <heelandyj1@gmail.com>
2020-08-09 18:00:11 -0300

    create the back-end server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
-const express = require('express')()
-.get("/", (req, res)=>{
- return res.send("Hi form Mars")
-}).
-listen(5000)
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
+.get("/home/heel/Programa/projectweb/", (req, res)=>{
+ return res.sendFile(__dirname + "/home/heel/Programa/projectweb/view/index.html")
+})
+.get("/home/heel/Programa/projectweb/study" , (req, res) => {
+    return res.sendFile(__dirname + "/home/heel/Programa/projectweb/view/study.html")
+})
+.get("/home/heel/Programa/projectweb/page-give-class" , (req, res) => {
+    return res.sendFile(__dirname + "/home/heel/Programa/projectweb/view/study.html")
+})
+.listen(5500)
