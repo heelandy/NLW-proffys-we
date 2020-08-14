4d9c6aa4cdcdc7d0edbf2e6490d14bf4704d97e1
heelandy <heelandyj1@gmail.com>
2020-08-14 15:59:20 -0300

    changying features and correct sintax error

--- src/server.js
+++ src/server.js
@@ -1,16 +1,67 @@
+const proffys =[
+    { name:"Diego Fernadez",
+     avatar:"", 
+     whatsapp:"659974669",
+     bio:"Entusiasta das melhores technologia de quimica avencada",
+     subject:"Quimica",
+     cost:"20",
+     weekday:[0],
+     time_from:[ 720 ],
+     time_to:[1220]
+            
+    }
+    ]           
+const subjects = [
+    
+    "Artes",
+    "Biologia",
+    "Educacao Fisica",
+    "Fisica",
+    "Geografia",
+    "Historia",
+    "Matematica",
+    "Portugues",
+    "Quimica", 
+] 
+
+const weekdays = [
+ "Domingo",
+ "Segunda",
+ "Terca",
+ "Quarta",
+ "Quinta",
+"sexta",
+"Sabado",
+]
+
+
+
+
 const express = require('express')
 const server = express()
 
-server.use(express.static("public"))
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
+}
+function pageGiveClass(req, res) {
+    res.render("page-give-class.html")
+}
+//configurando o nunjucks
+const nunjucks = require('nunjucks') 
+nunjucks.configure("src/view", {
+    express: server,
+    noCache:true,
+})
 
-.get("/", (req, res)=>{
- return res.sendFile(__dirname + "/view/index.html")
-})
-.get("/study" , (req, res) => {
-    return res.sendFile(__dirname + "/view/study.html")
-})
-.get("/page-give-class" , (req, res) => {
-    return res.sendFile(__dirname + "/view/page-give-class.html")
-})
+// configurar arquivos estaticos css
+server.use(express.static("public"))
+//rotas da applicaÇão
+.get("/", pageLanding)
+.get("/study" ,pageStudy)
+.get("/page-give-class" ,pageGiveClass)
 .listen(5000)
 
